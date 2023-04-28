e9fd0f933762c99673e37e453344e3ac804005ee
Amity_Resources <poovarasan@mindwaveventures.com>
2023-04-28 15:24:50 +0530

    fouth time commit

--- Maven_project/src/main/java/com/sample/Maven_project/Sample.java
+++ Maven_project/src/main/java/com/sample/Maven_project/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 //		System.out.println("poovarasan testing");
-		System.out.println("poovarasan testingjkwdyh");
+//		System.out.println("poovarasan testingjkwdyh");
 	}
 
 }
