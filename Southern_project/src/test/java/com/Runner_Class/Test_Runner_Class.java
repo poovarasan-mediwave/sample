e6bc3494c30acd65affaa13ec3e396aae7cd655f
Amity_Resources <poovarasan@mindwaveventures.com>
2023-05-02 12:18:37 +0530

    first commmmmmit

---
+++ Southern_project/src/test/java/com/Runner_Class/Test_Runner_Class.java
@@ -0,0 +1,16 @@
+package com.Runner_Class;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src//test//resources//Feature_file", monochrome = true, dryRun = true, glue = {
+		"com.Step_Definition" }, plugin = { "html:Report/HtmlReport", "pretty", "json:JansonReport/Report.json",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })
+public class Test_Runner_Class {
+	public static void main(String[] args) {
+		System.out.println("dummy text");
+	}
+}
